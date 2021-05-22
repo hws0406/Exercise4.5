1e0dc6808677283ac26a226aa550ee522bbe4636
Ho Wee Sern <72723621+hws0406@users.noreply.github.com>
2021-05-23 03:31:55 +0800

    Create Mango.java

---
+++ Mango.java
@@ -0,0 +1,20 @@
+public class Mango extends Fruit{
+	Scanner scan = new Scanner(System.in);
+	private String size;
+	private int weight;
+	
+	public Mango(String name, String s, int w) {
+		super(name, quantity , price);
+		System.out.println("Mango constructor is invoked");
+		this.size = s;
+		this.weight = w;
+	}
+	
+	public String sizeMango() {
+		return this.size;
+	}
+	
+	public int weightMango() {
+		return this.weight;
+	}
+}
